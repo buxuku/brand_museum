f2395d2ba5913096b904c1f84d9c69e189345ddd
林晓东 <buxuku@gmail.com>
2017-07-24 14:52:48 +0800

    read comtent

---
+++ list.php
@@ -0,0 +1,39 @@
+<?php
+$file = 'a.txt'; 
+$content = file_get_contents($file); 
+
+$array = explode("\r\n", $content); 
+$brand = array();
+$brandList=array();
+for($i=0; $i<count($array); $i++) 
+{ 
+	$row=$array[$i];
+	$brand_name=explode(',', $row);
+	$brandIdList=explode(',', $row,3);
+	$brandList[$brand_name[1]]=$brandIdList[2];
+	if(isset($brand[$brand_name[0]])){
+		Array_push($brand[$brand_name[0]],$brand_name[1]);
+	}else{
+		$brand[$brand_name[0]]=array($brand_name[1]);
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+     <head>
+         <title><?php echo "222" ?></title>
+     </head>
+     <body>
+     	<?php
+     		foreach($brand as $key=>$value){
+     		?>
+     		<p><?php echo $key?></p>
+     		<?php	for($j=0;$j<count($value);$j++){
+     	?>
+         	<span><?php echo $value[$j] ?></span>
+        <?php 
+        		}
+        	} 
+        ?>
+     </body>
+</html>
