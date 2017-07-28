c2fa1c0f3e824379b7e5363530aab07fffcabedd
林晓东 <buxuku@gmail.com>
2017-07-28 20:08:11 +0800

    resize the images

--- js/brandDetail.js
+++ js/brandDetail.js
@@ -6,7 +6,21 @@ mui.ready(function() {
 		var goodsImg=document.querySelectorAll(".gaia-brandDetail-content .mui-card-content img");
 		goodsImg.forEach(function(item){
 			item.style.width=bodyWidth+"px";
-			item.style.height=bodyWidth+"px";
+			var img = new Image();
+				// 改变图片的src
+				img.src = item.getAttribute('src');
+				var check = function(){
+					if(img.width!=0){
+						item.style.marginTop=(-(item.offsetWidth/img.width*img.height-item.offsetWidth)/2)+'px';
+					}
+				};
+				var set = setInterval(check,40);
+				// 加载完成获取宽高
+				img.onload = function(){
+				    item.style.marginTop=(-(item.offsetWidth/img.width*img.height-item.offsetWidth)/2)+'px';
+				    // 取消定时获取宽高
+				    clearInterval(set);
+				};
 		});
 	}
 	window.onresize=setSize;
