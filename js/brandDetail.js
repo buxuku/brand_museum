e364cca3f89fc19eae487d43e3d33678dd8f5aa9
yeyangzi <1501421196@qq.com>
2017-07-31 20:46:22 +0800

    详情页js和详情页图片超出隐藏添加

--- js/brandDetail.js
+++ js/brandDetail.js
@@ -1,6 +1,17 @@
 mui.init();
 mui.ready(function() {
+	var ad=document.querySelector(".ad");
+	var adClose=document.querySelector(".gaia-ad-close");
+	/*先设定初值宽高*/
 	setSize();
+	/*在设置位置初始位置：默认影藏*/
+	adDown();
+	/*这里添加判断，如果不是我们自己的添加广告*/
+	setTimeout(adUp,1000);/*页面加载完毕之后1s后广告出现*/
+	
+	/*点击关闭ad隐藏*/
+	adClose.onclick=adDown;
+	
 	function setSize(){
 		var bodyWidth=(document.body.offsetWidth-38)/3;
 		var goodsImg=document.querySelectorAll(".gaia-brandDetail-content .mui-card-content img");
@@ -22,6 +33,30 @@ mui.ready(function() {
 				    clearInterval(set);
 				};
 		});
+	/*广告*/
+		ad.style.height=ad.offsetWidth/20*3+'px';
+		ad.style.padding=ad.offsetWidth/200*3+'px';
+		/*广告close*/
+		var adClose=document.querySelector(".gaia-ad-close-box");
+		var adDownload=document.querySelector(".gaia-ad-download-box");
+		var adText=document.querySelector(".gaia-ad-text");
+		
+		var adLineheight=ad.offsetHeight-ad.offsetWidth/200*6;
+		
+		adClose.style.lineHeight=adLineheight+'px';
+		adDownload.style.lineHeight=adLineheight+'px';
+		adText.style.paddingTop=(adLineheight-adText.offsetHeight)/2+'px';
 	}
+	
+	function adDown(){
+		ad.style.transition='bottom linear 0.2s'; 
+		ad.style.bottom=-ad.offsetHeight+'px';
+	}
+	
+	function adUp(){
+		ad.style.transition='bottom ease-out 0.2s'; 
+		ad.style.bottom=0+'px';
+	}
+	
 	window.onresize=setSize;
 })
