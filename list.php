b38a4537f567f55f80404ab3abb89f2ff93c3ca9
yeyangzi <1501421196@qq.com>
2017-08-10 16:26:18 +0800

    列表页添加垂直居中解构

--- list.php
+++ list.php
@@ -50,6 +50,7 @@ mysqli_query($conn,'set names utf8');
 												<div class="mui-card">
 													<a href="goods_list.php?brand_id=<?php echo $row['id'] ?>&brand_name=<?php echo urlencode($row['show_name']) ?>">
 														<div class="mui-card-content">
+															<span class="vertical"></span>
 															<img src="<?php echo $row['logo_path']?IMG_PREFIX.getThumbImg($row['logo_path'],400):'img/default_img.jpg'; ?>"/>
 														</div>
 														<div class="gaia-card-footer"><?php echo $row['show_name'] ?></div>
