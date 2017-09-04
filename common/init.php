a34be3ac590d93eefae26c4ac5b1db5e0eafa2c2
林晓东 <buxuku@gmail.com>
2017-09-04 12:07:17 +0800

    Realize no goods id in brand.txt and use locate for order by

--- common/init.php
+++ common/init.php
@@ -12,17 +12,16 @@ for($i=0; $i<count($array); $i++)
 	$row=$array[$i];
 	$brand_name=explode(',', $row);
 	$brandIdList=explode(',', $row,3);
-	if(count($brandIdList)<3){
+	if(count($brandIdList)<2){
 		continue;
 	}
-	$brandList[$brand_name[1]]=$brandIdList[2];
+	$brandList[intval($brand_name[1])]=count($brandIdList)>2?$brandIdList[2]:"";
 	if(isset($brand[$brand_name[0]])){
 		Array_push($brand[$brand_name[0]],$brand_name[1]);
 	}else{
 		$brand[$brand_name[0]]=array($brand_name[1]);
 	}
 }
-
 function getThumbImg($path,$size){
 	$size=$size?$size:400;
 	$imgInfo=pathinfo($path);
--- goods_list.php
+++ goods_list.php
@@ -20,7 +20,8 @@ function getStoreDivided($price,$settlement,$ratio){
 	}
 	return $storeDivided;
 }
-$sql="SELECT c.id,c.article_number, c.`name`, c.price,c.ratio, c.settlement, pic.goods_pics, b.show_name FROM commodity AS c LEFT JOIN ( SELECT GROUP_CONCAT(p.path) AS goods_pics, p.commodity_id AS goods_id FROM goods_pictures AS p WHERE p.type = 'goods' GROUP BY p.commodity_id ) AS pic ON pic.goods_id = c.id LEFT JOIN brand AS b ON c.brand_id = b.id WHERE c.id IN (".$brandList[$brand_id].")";
+$ext_where=$brandList[$brand_id]?" c.id IN ($brandList[$brand_id]) ORDER BY locate(c.id,'$brandList[$brand_id]')":" b.id=".$brand_id;
+$sql="SELECT c.id,c.article_number, c.`name`, c.price,c.ratio, c.settlement, pic.goods_pics, b.show_name FROM commodity AS c LEFT JOIN ( SELECT GROUP_CONCAT(p.path) AS goods_pics, p.commodity_id AS goods_id FROM goods_pictures AS p WHERE p.type = 'goods' GROUP BY p.commodity_id ) AS pic ON pic.goods_id = c.id LEFT JOIN brand AS b ON c.brand_id = b.id WHERE ".$ext_where;
 $result = mysqli_query($conn,$sql);
 if(!$result){
 	die("该品牌下暂无商品!");
