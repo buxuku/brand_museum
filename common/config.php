fefcf05d47fad60c59d1e90fe88c654f18fde500
林晓东 <buxuku@gmail.com>
2017-11-10 15:34:14 +0800

    change config file

--- common/config.php
+++ common/config.php
@@ -1,10 +1,12 @@
 <?php
+//数据库连接信息
 $mysql_server_name='192.168.1.17';
 $mysql_username='root';
 $mysql_password='86da06358I*';
 $mysql_database='retail_1101';
 $mysql_port='3306';
 
-define("IMG_PREFIX","//uploaded.gaiasys.cn/retail/images/");
-define("DATA_ROOT","http://192.168.1.18:8080/data/");
+define("IMG_PREFIX","//uploaded.gaiasys.cn/retail/images/");//图片根地址
+define("DATA_ROOT","http://192.168.1.18:8080/data/");//排行榜数据源根地址
+define("BRAND_MUSEUM_ACTIVITY_ID",9);//品牌馆活动id
 
--- common/init.php
+++ common/init.php
@@ -1,10 +1,36 @@
 <?php
-require("config.php");
+require("common.php");
 
-function getThumbImg($path,$size){
-	$size=$size?$size:400;
-	$imgInfo=pathinfo($path);
-	$imgext=$imgInfo['extension'];
-	$imgUrl=str_replace($imgext,$size.".".$imgext,$path);
-	return $imgUrl;
+$conn=mysqli_connect($mysql_server_name,$mysql_username,$mysql_password,$mysql_database,$mysql_port) or die("error connecting");
+mysqli_query($conn,'set names utf8');
+
+$sql ="select * from activity_description where activity_id=".BRAND_MUSEUM_ACTIVITY_ID;
+
+$result = mysqli_query($conn,$sql);
+if($result){
+	$array=array();
+	while($row = mysqli_fetch_array($result)){
+		if($row['name'] && $row['content']){
+			$array[]=$row['name'].",".$row['content'].",".$row['note'];
+		}
+	}
+}else{
+	die("没有查询到活动数据");
+}
+$brand = array();
+$brandList=array();
+for($i=0; $i<count($array); $i++)
+{ 
+	$row=$array[$i];
+	$brand_name=explode(',', $row);
+	$brandIdList=explode(',', $row,3);
+	if(count($brandIdList)<2){
+		continue;
+	}
+	$brandList[intval($brand_name[1])]=count($brandIdList)>2?$brandIdList[2]:"";
+	if(isset($brand[$brand_name[0]])){
+		Array_push($brand[$brand_name[0]],$brand_name[1]);
+	}else{
+		$brand[$brand_name[0]]=array($brand_name[1]);
+	}
 }
